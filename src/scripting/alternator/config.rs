cda6ebcb3530a5c11b475514ec5af2a2d2dac165
Michał Birecki <54212870+stopnoanime@users.noreply.github.com>
2026-03-24 09:51:01 +0100

    Add credentials to Alternator's DbConnectionConf and use them in connect
    The config allows for specifying the credentials as command line arguments.
    By specifying the aws-credentials option the SDK will fetch credentials from the environment.
    
    (cherry picked from commit 1d37fa931a4efbb1ddee29390f1ae1b321acfcb0)

--- src/scripting/alternator/config.rs
+++ src/scripting/alternator/config.rs
@@ -2,4 +2,21 @@ use clap::Parser;
 use serde::{Deserialize, Serialize};
 
 #[derive(Parser, Debug, Default, Serialize, Deserialize)]
-pub struct DbConnectionConf {}
+pub struct DbConnectionConf {
+    /// Use AWS credentials and region from the environment.
+    /// Mutually exclusive with `access-key-id`, `secret-access-key` and `region`.
+    #[clap(long("aws-credentials"), conflicts_with_all = &["access_key_id", "secret_access_key", "region"])]
+    pub aws_credentials: bool,
+
+    /// Access key ID.
+    #[clap(long("access-key-id"), default_value = "")]
+    pub access_key_id: String,
+
+    /// Secret access key.
+    #[clap(long("secret-access-key"), default_value = "")]
+    pub secret_access_key: String,
+
+    /// Region.
+    #[clap(long("region"), default_value = "us-east-1")]
+    pub region: String,
+}
--- src/scripting/alternator/connect.rs
+++ src/scripting/alternator/connect.rs
@@ -10,14 +10,32 @@ use aws_sdk_dynamodb::Client;
 pub async fn connect(conf: &ConnectionConf) -> Result<Context, AlternatorError> {
     let address = conf.addresses.first().cloned().unwrap_or_default();
 
-    // TODO: use latte parameters for setting the configuration
-    let config = aws_config::defaults(BehaviorVersion::latest())
+    let mut config_loader = aws_config::defaults(BehaviorVersion::latest())
         .endpoint_url(&address)
-        .region(Region::new("us-east-1"))
-        .credentials_provider(Credentials::new("", "", None, None, ""))
         .retry_config(RetryConfig::standard().with_max_attempts(1))
-        .load()
-        .await;
+        .timeout_config(
+            aws_config::timeout::TimeoutConfig::builder()
+                .operation_timeout(conf.request_timeout)
+                .build(),
+        );
+
+    // We only specify custom credentials if aws_credentials flag is not set.
+    // If aws_credentials flag is set, the SDK will automatically use credentials from the environment.
+    if !conf.db.aws_credentials {
+        let creds = Credentials::new(
+            &conf.db.access_key_id,
+            &conf.db.secret_access_key,
+            None,
+            None,
+            "",
+        );
+
+        config_loader = config_loader
+            .credentials_provider(creds)
+            .region(Region::new(conf.db.region.clone()));
+    }
+
+    let config = config_loader.load().await;
 
     let client = Client::new(&config);
 
